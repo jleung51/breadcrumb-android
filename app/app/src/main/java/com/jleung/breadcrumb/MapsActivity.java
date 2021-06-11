3677621a8da12fef1f0568ee8cf40ebd06af2af6
Jeffrey Leung <jleung513@gmail.com>
2021-06-11 12:45:10 -0700

    Update template information to new app

---
+++ app/app/src/main/java/com/jleung/breadcrumb/MapsActivity.java
@@ -0,0 +1,132 @@
+package com.jleung.breadcrumb;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final String TAG = MapsActivity.class.getName();
+
+    private GoogleMap mMap;
+
+    // TODO: Set the start location and zoom here
+    private LatLng startLocation = new LatLng(39.952090, -75.190840);
+    @SuppressWarnings("FieldCanBeLocal")
+    private float startZoom = 17.5f;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            Toast.makeText(
+                    this,
+                    "Failed to create a SupportMapFragment for the Google Maps.",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+        else {
+            mapFragment.getMapAsync(this);
+        }
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        addHeatMap();
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.google_maps_styling));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        mMap.addMarker(new MarkerOptions().position(startLocation).title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(startLocation));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(startZoom));
+    }
+
+    private void addHeatMap() {
+        List<LatLng> list;
+
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.location_heatmap_data);
+        } catch (JSONException e) {
+            Toast.makeText(
+                    this, "Problem reading list of locations.", Toast.LENGTH_LONG
+            ).show();
+            return;
+        }
+
+        // Create a heat map tile provider, passing it the lats and longs.
+        HeatmapTileProvider mProvider = new HeatmapTileProvider
+                .Builder()
+                .data(list)
+                .build();
+
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
+    }
+
+}
+
---
+++ app/app/src/main/java/com/jleung/breadcrumb/onboarding/Onboarding1Fragment.java
@@ -0,0 +1,23 @@
+package com.jleung.breadcrumb.onboarding;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.jleung.breadcrumb.R;
+
+// Fragment must be a public static class
+public class Onboarding1Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(
+                R.layout.fragment_onboarding_1, container, false
+        );
+    }
+
+}
